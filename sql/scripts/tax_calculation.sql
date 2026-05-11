32a885ca19c316faaae2556f4fb5acd533bc1b9b
rkurbanov <kurbanov_roman@icloud.com>
2026-05-12 00:30:49 +0400

    Replaced Cities table with City

--- sql/scripts/tax_calculation.sql
+++ sql/scripts/tax_calculation.sql
@@ -13,16 +13,16 @@ SELECT TAX_BRACKET.id,
              ELSE UPPER_TAX_BRACKET.tax_from
            END) - TAX_BRACKET.tax_from)                                    AS taxable_income,
 
-       ((CASE
+       LEAST(((CASE
              WHEN (UPPER_TAX_BRACKET IS NULL OR :salary < UPPER_TAX_BRACKET.tax_from)
                  THEN :salary
              ELSE UPPER_TAX_BRACKET.tax_from
-           END) - TAX_BRACKET.tax_from) * (TAX_BRACKET.percent_from / 100) AS tax_owed
+           END) - TAX_BRACKET.tax_from) * (TAX_BRACKET.percent_from / 100), TAX_BRACKET.tax_limit) AS tax_owed
 
 FROM TAX AS TAX_BRACKET
          JOIN Country ON Country.id = TAX_BRACKET.country_id
          JOIN Province ON Province.country_id = Country.id
-         JOIN Cities ON Cities.province_id = Province.id
+         JOIN city ON city.province_id = Province.id
          LEFT JOIN LATERAL (SELECT *
                             FROM TAX UPPER_TAX
                             WHERE UPPER_TAX.country_id = TAX_BRACKET.country_id
@@ -35,9 +35,9 @@ FROM TAX AS TAX_BRACKET
                             ORDER BY UPPER_TAX.tax_from
                             LIMIT 1) AS UPPER_TAX_BRACKET
                    ON TRUE
-WHERE Cities.name = :cityName
+WHERE city.name = :cityName
   AND (TAX_BRACKET.province_id = Province.id OR TAX_BRACKET.province_id IS NULL)
-  AND (TAX_BRACKET.city_id = Cities.id OR TAX_BRACKET.city_id IS NULL)
+  AND (TAX_BRACKET.city_id = city.id OR TAX_BRACKET.city_id IS NULL)
   AND TAX_BRACKET.tax_from < :salary
 ORDER BY TAX_BRACKET.country_id, TAX_BRACKET.province_id, TAX_BRACKET.city_id, TAX_BRACKET.jurisdiction,
          TAX_BRACKET.tax_from;
